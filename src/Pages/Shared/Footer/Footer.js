311bf36286c4eca3022738978306cd59b60833b0
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-10-26 20:38:23 +0600

    footer-update

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div className=' bg-black p-5 mt-5 text-center'>
-            <p className=' text-white'>Copyright @2023 comming soon</p>
+            <p className=' text-white'>All rights reserved by CodeScripts @ 2022 </p>
+            <div>
+                <span className=' me-3 '><FaFacebook></FaFacebook></span>
+                <span className=' me-3'><FaLinkedinIn></FaLinkedinIn></span>
+                <span className=' me-3'><FaInstagram></FaInstagram></span>
+            </div>
         </div>
     );
 };
